03fe588ba46ed6a5eaedf523b65ecdccfcefce33
Arsela Bita <arselabita@gmail.com>
2025-12-02 15:57:18 +0100

    currently working for the quotation check

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -10,6 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
+#include "minishell.h"
 
+/*
+	A parser is divided into two parts: a Lexical Analyzer or Lexer takes the input characters and 
+puts the characters together into words called tokens, and a Parser that processes the 
+tokens according to a grammar and build the command table.
 
+*/
+
+int tokenizer(char *line)
+{
+	int i;
+	char c;
+	t_state state;
+	t_token *toklist;
+
+	i = 0;
+	state = STATE_NORMAL;
+	while (line[i])
+	{
+		c = line[i];
+		if (state == STATE_NORMAL)
+		{
+			if (c == '\'')
+				state = STATE_SINGLE_QUOTE;
+			else if (c == '"')
+				state = STATE_DOUBLE_QUOTE;	
+		}
+		i++;
+	}
+}
