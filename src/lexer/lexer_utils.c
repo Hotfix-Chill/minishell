ebc47dd8b4fdd161ef619534341c2e85b2491b5e
Arsela Bita <arselabita@gmail.com>
2025-12-05 15:41:49 +0100

    added the ft_isspace unct

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -24,3 +24,10 @@ int	skip_whitespaces(char *line, int i)
 	}
 	return (i); // return the index of the first non whitespace character
 }
+int ft_isspace(int c)
+{
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
+		return (1);
+	else	
+		return (0);
+}
