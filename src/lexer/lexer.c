71ec844f63145d553ea5af1d538e6357d966abe8
Arsela Bita <arselabita@gmail.com>
2025-12-05 15:41:10 +0100

    working on collecting words after defining the quote state

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -20,6 +20,61 @@ tokens according to a grammar and build the command table.
 
 */
 
+static int append_char_to_content(t_token *toklist, char c)
+{
+	char *new_content;
+	char *old_content;
+	char char_str[2];
+
+	char_str[0] = c;
+	char_str[1] = '\0';
+
+	old_content = toklist->content;
+	new_content = ft_strjoin(old_content, char_str); // if not null join the existing str
+	if (!new_content)
+		return (-1); // ALLOCATION_FAILED create an enum to clean up
+	if (toklist->content) // here i free the old contetn and update the pointer
+		free(toklist->content);
+	toklist->content = new_content;
+	return (EXIT_SUCCESS);
+}
+
+static int collect_quote_content(char *line, int *i_ptr, t_token *toklist)
+{
+	int prev_quote;
+	char c;
+	int i;
+
+	i = *i_ptr;
+	while (line[i])
+	{
+
+		// check if we hit a boundary (space, pipe, redir, eof)
+		c = line[i];
+		if (toklist->quote == QUOTE_NORMAL)
+		{
+			if ((ft_isspace(c) || c == '|' || c == '<' || c == '>'))
+				break ; // stop collecting if outside the quotes
+		}
+		// check if we hit a quote that changes the state (i.e. closes the quote)
+		// cases like ('word'abc), it must also be checked if it ends the quote
+		prev_quote = toklist->quote;
+		def_quote_state(line, &i, toklist);
+		if (prev_quote != toklist->quote)
+		{
+			if (toklist->quote == QUOTE_NORMAL)
+				break ;
+			continue ; // state was changed, returned error or new quote and brake cus i now is incremented to the next
+		}
+		if (append_char_to_content(toklist, line[i]) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		i++;
+	}
+	*i_ptr = i;
+	return (EXIT_SUCCESS);
+}
+
+
 static int	def_quot_state(char *line, int *i_ptr, t_token *toklist)
 {
 	char	c;
@@ -31,17 +86,39 @@ static int	def_quot_state(char *line, int *i_ptr, t_token *toklist)
 	next_c = line[i + 1];
 	// state 1: we are not currently inside quotes (QUOTE_NORMAL)
 	normal_quote_state(c, next_c, i, toklist);
-	// state 2: we are currently inside double quotes (QUOTE_DOUBLE)
 
-	// else if (toklist->quote == QUOTE_DOUBLE)
-	// {
-	// 	// if found collect till the next one; set a flag
-	// 	// on unmatched quotes return error, and free
-	// }
-	// // state 3: we are currently inside single quotes (QUOTE_SINGLE)
-	// else if (toklist->quote == QUOTE_SINGLE)
-	// {
-	// }
+	// state 2: we are currently inside double quotes (QUOTE_DOUBLE)
+	// double_quote_state(c, i, toklist);
+	if (toklist->quote == QUOTE_DOUBLE)
+	{
+		// if found collect till the next one; set a flag
+		// on unmatched quotes return error, and free
+		
+		// found the closing quote
+		if (c == '"')
+		{
+			toklist->quote = QUOTE_NORMAL;
+			i++;
+		}
+		else if (c )
+
+	}
+
+	// state 3: we are currently inside single quotes (QUOTE_SINGLE)
+	// single_quote_state(c, i, toklist);
+	else if (toklist->quote == QUOTE_SINGLE)
+	{
+		// found the closing quote
+		if (c == '\'')
+		{
+			toklist->quote = QUOTE_NORMAL;
+			i++;
+		}
+		else
+		{
+
+		}
+	}
 
 	*i_ptr = i;
 	return (EXIT_SUCCESS);
@@ -75,7 +152,7 @@ t_token	*tokenizer(const char *line)
 		if (toklist->typ == TOKEN_WORD)
 			// iterate i till it hits a space or pipe quote
 			// and allocate toklist->content
-			i = collect_word_content(line, i, toklist);
+			i = collect_word_content(line, &i, toklist);
 		else if (toklist->quote != QUOTE_NORMAL)
 			i = collect_word_content(line, i, toklist);
 		// add_token_to_list(&head, toklist);
