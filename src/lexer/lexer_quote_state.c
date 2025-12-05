d08575b70a882e76cc959b2c7c8eb52e1d02dfd3
Arsela Bita <arselabita@gmail.com>
2025-12-05 22:04:55 +0100

    commented a thinf

--- src/lexer/lexer_quote_state.c
+++ src/lexer/lexer_quote_state.c
@@ -12,53 +12,54 @@
 
 #include "minishell.h"
 
-void normal_quote_state(char c, char next_c, int i, t_token *toklist)
-{
-	if (toklist->quote == QUOTE_NORMAL)
-	{
-		if (c == '>' && next_c == '>')
-		{
-			toklist->typ = TOKEN_REDIR;
-			toklist->redir = REDIR_APPEND;
-			i += 2;
-		}
-		else if (c == '<' && next_c == '<')
-		{
-			toklist->typ = TOKEN_REDIR;
-			toklist->redir = REDIR_HEREDOC;
-			i += 2;
-		}
-		else if (c == '\'')
-		{
-			toklist->quote = QUOTE_SINGLE;
-			i++;
-		}
-		else if (c == '\"')
-		{
-			toklist->quote = QUOTE_DOUBLE;
-			i++;
-		}
-		else if (c == '|')
-		{
-			toklist->typ = TOKEN_PIPE;
-			i++;
-		}
-		else if (c == '>')
-		{
-			toklist->typ = TOKEN_REDIR;
-			toklist->redir = REDIR_OUT;
-			i++;
-		}
-		else if (c == '<')
-		{
-			toklist->typ = TOKEN_REDIR;
-			toklist->redir = REDIR_IN;
-			i++;
-		}
-		else
-			toklist->typ = TOKEN_WORD;
-	}
-}
+// int normal_quote_state(char c, char next_c, int i, t_token *toklist)
+// {
+// 	if (toklist->quote == QUOTE_NORMAL)
+// 	{
+// 		if (c == '>' && next_c == '>')
+// 		{
+// 			toklist->typ = TOKEN_REDIR;
+// 			toklist->redir = REDIR_APPEND;
+// 			i += 2;
+// 		}
+// 		else if (c == '<' && next_c == '<')
+// 		{
+// 			toklist->typ = TOKEN_REDIR;
+// 			toklist->redir = REDIR_HEREDOC;
+// 			i += 2;
+// 		}
+// 		else if (c == '\'')
+// 		{
+// 			toklist->quote = QUOTE_SINGLE;
+// 			i++;
+// 		}
+// 		else if (c == '\"')
+// 		{
+// 			toklist->quote = QUOTE_DOUBLE;
+// 			i++;
+// 		}
+// 		else if (c == '|')
+// 		{
+// 			toklist->typ = TOKEN_PIPE;
+// 			i++;
+// 		}
+// 		else if (c == '>')
+// 		{
+// 			toklist->typ = TOKEN_REDIR;
+// 			toklist->redir = REDIR_OUT;
+// 			i++;
+// 		}
+// 		else if (c == '<')
+// 		{
+// 			toklist->typ = TOKEN_REDIR;
+// 			toklist->redir = REDIR_IN;
+// 			i++;
+// 		}
+// 		else
+// 			toklist->typ = TOKEN_WORD;
+// 	}
+// 	return (i);
+// }
 
 // void double_quote_state()
 // {
