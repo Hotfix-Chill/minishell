868ca42cc022ef24e86be3e97d3acde8b5a6e139
Arsela Bita <arselabita@gmail.com>
2025-12-09 19:50:36 +0100

    here i just removed the ft_isspace

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/03 14:26:29 by abita             #+#    #+#             */
-/*   Updated: 2025/12/03 14:26:31 by abita            ###   ########.fr       */
+/*   Created: 2025/12/09 17:04:12 by abita             #+#    #+#             */
+/*   Updated: 2025/12/09 17:04:13 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 
 int	skip_whitespace(char *line, int i)
 {
-	while (line[i] && line[i] == ' ' || (line[i] >= 9 && line[i] <= 13))
+	while ((line[i] && line[i] == ' ') || (line[i] >= 9 && line[i] <= 13))
 			i++;
 	return (i);
-}
-int ft_isspace(int c)
-{
-	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
-		return (1);
-	else	
-		return (0);
 }
