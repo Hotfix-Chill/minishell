4c4f0b640c435016572d1dcce88685222791ef09
Arsela Bita <arselabita@gmail.com>
2025-12-17 23:40:49 +0100

    create and init list, cmd and add cmd to list and add token or arg now to cmd arr

--- src/parser/parser_cmd.c
+++ src/parser/parser_cmd.c
@@ -39,19 +39,19 @@ t_cmds *create_cmds(void)
 	return (cmd);
 }
 
-int add_cmd_to_list(t_stack *lst, t_cmds *node)
+int add_cmd_to_list(t_stack *lst, t_cmds *cmd)
 {
-	if (!lst || !node)
+	if (!lst || !cmd)
 		return (-1);
 	if (!lst->head)
 	{
-		lst->head = node;
-		lst->tail = node;
+		lst->head = cmd;
+		lst->tail = cmd;
 	}
 	else
 	{
-		lst->tail->next = node;
-		lst->tail = node;
+		lst->tail->next = cmd;
+		lst->tail = cmd;
 	}
 	lst->size++;
 	return (EXIT_SUCCESS);
@@ -68,8 +68,31 @@ int count_args(char **args)
 	return (count);
 }
 
-// Add an argument to command's argv array
-int add_arg_to_cmd(t_cmds *cmd, const char *arg)
+// Add the token content like words to command's argv array
+int add_arg_to_cmd(t_cmds *curr_cmd, const char *tok_content)
 {
-	
+	int arg_count;
+	int i;
+	char **new_argv;
+
+	if (!curr_cmd || !tok_content)
+		return (NULL);
+	arg_count = count_args(tok_content);
+	new_argv = (char **)ft_calloc(arg_count + 2, sizeof(char *));
+	if (!new_argv)
+		return (NULL);
+	i = 0;
+	while (curr_cmd->argv[i])
+	{
+		new_argv[i] = curr_cmd->argv[i];
+		i++;
+	}
+	// curr_cmd->argv = new_argv;
+	// new_argv = '\0';
+	return (EXIT_SUCCESS);
 }
+
+// int add_redir_to_cmd(t_cmds *cmd, t_redir *redir)
+// {
+
+// }
