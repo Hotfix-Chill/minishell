09678a75e1e289a4b3d2069eaa16d296473ddc6b
Arsela Bita <arselabita@gmail.com>
2026-01-06 00:19:15 +0100

    the rest of helper funct

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -18,7 +18,23 @@ int	skip_whitespace(char *line, int i)
 			i++;
 	return (i);
 }
+int pipe_check(char *line, int *i_ptr, t_token *tok)
+{
+	int		i;
+	char	next_c;
 
+	i = *i_ptr;
+	next_c = line[i + 1];
+	if (line[i] == '|')
+	{
+		if (line[i + 1] == '|')
+			return (REDIR_ERROR);
+		tok->typ = TOKEN_PIPE;
+		*i_ptr = i + 1;
+		return (REDIR_FOUND);
+	}
+	return (REDIR_NONE);
+}
 int redir_check(char *line, int *i_ptr, t_token *tok)
 {
 	int		i;
@@ -45,4 +61,51 @@ int redir_check(char *line, int *i_ptr, t_token *tok)
 		return (REDIR_FOUND);
 	}
 	return (REDIR_NONE);
+}
+int	decide_token_type(char *line, int *i_ptr, t_token *tok)
+{
+	int		i;
+	int		res;
+	int res_pipe;
+
+	i = *i_ptr;
+	res = redir_check(line, i_ptr, tok);
+	if (res == REDIR_ERROR)
+		return (-1);
+	else if (res == REDIR_FOUND)
+		return (EXIT_SUCCESS);
+	res_pipe = pipe_check(line, i_ptr, tok);
+	if (res_pipe == REDIR_ERROR)
+		return (-1);
+	else if (res_pipe == REDIR_FOUND)
+		return (EXIT_SUCCESS);
+	else if (line[i] == '>')
+		return (tok->typ = TOKEN_REDIR, tok->redir = REDIR_OUT, *i_ptr = i + 1,
+			EXIT_SUCCESS);
+	else if (line[i] == '<')
+		return (tok->typ = TOKEN_REDIR, tok->redir = REDIR_IN, *i_ptr = i + 1,
+			EXIT_SUCCESS);
+	tok->typ = TOKEN_WORD;
+	tok->quote = QUOTE_NORMAL;
+	return (EXIT_SUCCESS);
+}
+/*
+	tokens contetnt setup this for non word tokens
+*/
+int set_operator_content(t_token *tok)
+{
+	if (tok->typ == TOKEN_PIPE)
+		return (append_str_to_content(tok, "|"));
+	if (tok->typ == TOKEN_REDIR)
+	{
+		if (tok->redir == REDIR_APPEND)
+			return (append_str_to_content(tok, ">>"));
+		if (tok->redir == REDIR_HEREDOC)
+			return (append_str_to_content(tok, "<<"));
+		if (tok->redir == REDIR_IN)
+			return (append_str_to_content(tok, "<"));
+		if (tok->redir == REDIR_OUT)
+			return (append_str_to_content(tok, ">"));
+	}
+	return (EXIT_SUCCESS);
 }
