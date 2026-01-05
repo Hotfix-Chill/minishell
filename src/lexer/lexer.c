70fc9c49d3e76d165019a0387a6305414e9b3c18
Arsela Bita <arselabita@gmail.com>
2026-01-06 00:18:27 +0100

    divided the funct bcs off lines, fixed leaks

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -19,63 +19,31 @@ puts the characters together into words called tokens,
 tokens according to a grammar and build the command table.
 
 */
-static int	decide_token_type(char *line, int *i_ptr, t_token *tok)
-{
-	int		i;
-	int		res;
 
-	i = *i_ptr;
-	res = redir_check(line, i_ptr, tok);
-	if (res == REDIR_ERROR)
-		return (-1);
-	else if (res == REDIR_FOUND)
-		return (EXIT_SUCCESS);
-	else if (line[i] == '|')
-		return (tok->typ = TOKEN_PIPE, *i_ptr = i + 1, EXIT_SUCCESS);
-	else if (line[i] == '>')
-		return (tok->typ = TOKEN_REDIR, tok->redir = REDIR_OUT, *i_ptr = i + 1,
-			EXIT_SUCCESS);
-	else if (line[i] == '<')
-		return (tok->typ = TOKEN_REDIR, tok->redir = REDIR_IN, *i_ptr = i + 1,
-			EXIT_SUCCESS);
-	tok->typ = TOKEN_WORD;
-	tok->quote = QUOTE_NORMAL;
-	return (EXIT_SUCCESS);
-}
-/*
-	tokens contetnt setup this for non word tokens
-*/
-static int set_operator_content(t_token *tok)
+static int token_check(const char *line, int *i, t_token *tok)
 {
-	if (tok->typ == TOKEN_PIPE)
-		return (append_str_to_content(tok, "|"));
-	if (tok->typ == TOKEN_REDIR)
+	if(decide_token_type((char *)line, i, tok) != EXIT_SUCCESS)
 	{
-		if (tok->redir == REDIR_APPEND)
-			return (append_str_to_content(tok, ">>"));
-		if (tok->redir == REDIR_HEREDOC)
-			return (append_str_to_content(tok, "<<"));
-		if (tok->redir == REDIR_IN)
-			return (append_str_to_content(tok, "<"));
-		if (tok->redir == REDIR_OUT)
-			return (append_str_to_content(tok, ">"));
+		printf("syntax error\n");
+		return (EXIT_FAILURE);
+	}
+	if (tok->typ == TOKEN_WORD)
+	{
+		if (collect_word_content((char *)line, i, tok) != EXIT_SUCCESS)
+			return (EXIT_FAILURE);
+	}
+	else
+	{
+		if (set_operator_content(tok) != EXIT_SUCCESS)
+			return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
-// main tokenizer
-// Lexer allocates every t_token node and each content string.
-// return head of list if success or NULL in case of error
-t_token_list	*tokenizer(const char *line)
+static int lexer_loop(const char *line, t_token_list *lst, t_token	*tok)
 {
 	int				i;
-	t_token			*tok;
-	t_token_list	*lst;
 
 	i = 0;
-	lst = init_token_list();
-	if (!lst)
-		return (NULL);
-	// toklist->quote = QUOTE_NORMAL;
 	while (line && line[i])
 	{
 		// a funct that advances i for each iter and checks for whitespaces
@@ -85,23 +53,40 @@ t_token_list	*tokenizer(const char *line)
 			break ;
 		tok = create_token();
 		if (!tok)
-			return (cleanup_all(lst, NULL));
-		// token definition function
-		decide_token_type((char *)line, &i, tok);
-		if (tok->typ == TOKEN_WORD)
 		{
-			if (collect_word_content((char *)line, &i, tok) != EXIT_SUCCESS)
-				return (cleanup_all(lst, tok));
+			cleanup_all(lst, NULL);
+			return (EXIT_FAILURE);
 		}
-		else
+		// token definition function
+		if (token_check(line, &i, tok) != EXIT_SUCCESS)
 		{
-			if (set_operator_content(tok) != EXIT_SUCCESS)
-				return (cleanup_all(lst, tok));
-
+			cleanup_all(lst, tok); //leak
+			return (EXIT_FAILURE);
 		}
 		// here i add to the list
 		if (add_token(lst, tok) != EXIT_SUCCESS)
-			return (cleanup_all(lst, tok));
+		{
+			cleanup_all(lst, tok);
+			return (EXIT_FAILURE);
+		}
 	}
-	return (lst);
+	return (EXIT_SUCCESS);
+}
+// main tokenizer
+// Lexer allocates every t_token node and each content string.
+// return head of list if success or NULL in case of error
+t_token_list	*tokenizer(const char *line)
+{
+	t_token_list	*lst;
+	t_token			*tok;
+
+	tok = NULL;
+	lst = init_token_list();
+	if (!lst)
+		return (NULL);
+	// toklist->quote = QUOTE_NORMAL;
+	if (lexer_loop(line, lst, tok) == EXIT_SUCCESS)
+		return (lst);
+	//cleanup_token(tok);
+	return NULL;
 }
