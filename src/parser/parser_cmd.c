0e67f453abb41b1cb8e0ea37d2bafb871b038769
Arsela Bita <arselabita@gmail.com>
2026-01-06 17:04:07 +0100

    data types for the alloc

--- src/parser/parser_cmd.c
+++ src/parser/parser_cmd.c
@@ -16,7 +16,7 @@ t_stack *init_cmd_list(void)
 {
 	t_stack *lst;
 
-	lst = (t_stack *)ft_calloc(1, sizeof(*lst));
+	lst = (t_stack *)ft_calloc(1, sizeof(t_stack));
 	if (!lst)
 		return (NULL);
 	lst->head = NULL;
@@ -29,7 +29,7 @@ t_cmds *create_cmds(void)
 {
 	t_cmds *cmd;
 
-	cmd = (t_cmds *)ft_calloc(1, sizeof(*cmd));
+	cmd = (t_cmds *)ft_calloc(1, sizeof(t_cmds));
 	if (!cmd)
 		return (NULL);
 	cmd->argv = NULL;
