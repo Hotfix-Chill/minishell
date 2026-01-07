d7847c4c254bafd5cb7a86b5b39be0cf31249dc4
Arsela Bita <arselabita@gmail.com>
2026-01-07 15:38:14 +0100

    fixed a bug and lines

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -10,13 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minishell.h"
-
-void print_all_commands(t_stack *cmd_list);
-void print_command(t_cmds *cmd, int index);
-void print_redirs(t_redirs *redirs);
-
 
 /*
 	P​arser that processes the tokens according to a grammar 
@@ -41,14 +35,59 @@ void print_redirs(t_redirs *redirs);
 
 		Token: "hello"
 		Before: current_cmd->argv = ["echo", NULL]
-		After:  current_cmd->argv = ["echo", "<<", "file name", NULL]
-		
-		*/
+		After:  current_cmd->argv = ["echo", "<<", "file name", NULL]		
+*/
+
+static int handle_pipe(t_token **tok_ptr, t_cmds **curr_cmd_ptr, t_stack *cmd_list)
+{
+	t_token *tok = *tok_ptr;
+	t_cmds *curr_cmd = *curr_cmd_ptr;
+
+	if (tok->typ == TOKEN_PIPE)
+	{
+		if (!curr_cmd->argv && !curr_cmd->redirs)
+			return (EXIT_FAILURE);
+		curr_cmd->pipe_after = true;
+		if (add_cmd_to_list(cmd_list, curr_cmd) != EXIT_SUCCESS)
+			return (EXIT_FAILURE);
+		curr_cmd = create_cmds();
+		if (!curr_cmd)
+			return (EXIT_FAILURE);
+		*curr_cmd_ptr = curr_cmd;
+		*tok_ptr = tok->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int parser_loop(t_token *tok, t_cmds *curr_cmd, t_stack *cmd_list)
+{
+	while (tok)
+	{
+		if (handle_pipe(&tok, &curr_cmd, cmd_list) != EXIT_SUCCESS)
+			return (EXIT_FAILURE);
+		else if(tok->typ == TOKEN_REDIR)
+		{
+			if (!tok->next || tok->next->typ != TOKEN_WORD)
+				return (EXIT_FAILURE);
+			if (add_redir_to_cmd(curr_cmd, tok, tok->next) != EXIT_SUCCESS)
+				return (EXIT_FAILURE);
+			tok = tok->next->next;
+		}
+		else if (tok->typ == TOKEN_WORD)
+		{
+			if (add_arg_to_cmd(curr_cmd, tok->content) != EXIT_SUCCESS)
+				return (EXIT_FAILURE);
+			tok = tok->next;
+		}
+		else 
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
 
 // taking the tooken list from the header
 t_stack *parsing(t_token_list *token)
 {
-	t_token			*tok;
 	t_cmds			*curr_cmd;
 	t_stack			*cmd_list;
 
@@ -57,27 +96,12 @@ t_stack *parsing(t_token_list *token)
 		return (NULL);
 	curr_cmd = create_cmds();
 	if (!curr_cmd)
-		return (NULL);
-	tok = token->head; 
-	while (tok)
-	{
-		if (tok->typ == TOKEN_PIPE)
-		{
-			curr_cmd->pipe_after = true;
-			if (add_cmd_to_list(cmd_list, curr_cmd) != EXIT_SUCCESS)
-				return (NULL);
-			curr_cmd = create_cmds();
-		}
-		else if(tok->typ == TOKEN_REDIR)
-		{
-			add_redir_to_cmd(curr_cmd, tok, tok->next);
-			tok = tok->next;
-		}
-		else if (tok->typ == TOKEN_WORD)
-			add_arg_to_cmd(curr_cmd, tok->content);
-		tok = tok->next;
-	}
-	if (add_cmd_to_list(cmd_list, curr_cmd) == EXIT_SUCCESS)
-		return (cmd_list);
-	return (NULL);
+		return (free(cmd_list), NULL);
+	if (parser_loop(token->head, curr_cmd, cmd_list) != EXIT_SUCCESS)
+		return (free_cmd_list(cmd_list), NULL);
+	if ((!curr_cmd->argv && !curr_cmd->redirs))
+		return (free_cmd_list(cmd_list), NULL);
+	if (add_cmd_to_list(cmd_list, curr_cmd) != EXIT_SUCCESS)
+		return (free_cmd_list(cmd_list), NULL);
+	return (cmd_list);
 }
