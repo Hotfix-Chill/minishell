e081511b62dbb45b1215e83592c1c44763575f4d
Arsela Bita <arselabita@gmail.com>
2026-01-08 17:48:03 +0100

    created the expansion file

---
+++ src/expansion/exp.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exp.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 14:21:56 by abita             #+#    #+#             */
+/*   Updated: 2026/01/08 14:21:57 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/*
+	Shell Parameter Expansion
+		The ‘$’ character introduces parameter expansion, command substitution, 
+	or arithmetic expansion. The parameter name or symbol to be expanded may be 
+	enclosed in braces, which are optional but serve to protect the variable 
+	to be expanded from characters immediately following it which could be 
+	interpreted as part of the name.
+*/
