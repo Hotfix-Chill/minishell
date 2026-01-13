48d8f8a3537f66dfec814070fa3edadeca4eaa7f
Patrick <no@mail.com>
2026-01-13 01:10:15 +0100

    To-Do: data->herdoc.count need to count all the heredocfiles in parser

--- src/executor/single_exec.c
+++ src/executor/single_exec.c
@@ -6,7 +6,7 @@
 /*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 00:42:02 by netrunner         #+#    #+#             */
-/*   Updated: 2026/01/12 22:46:13 by pjelinek         ###   ########.fr       */
+/*   Updated: 2026/01/13 00:52:42 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,14 +68,13 @@ void	exec_cmd(t_data *data, t_cmds *cmd)
 
 	i = 0;
 	init_signals_child();
-
-
 	if (!!cmd->redirs) // if redirs type is NULL dont go in
 		handle_redirections(data, cmd);
+	if (cmd->argv == NULL)
+		cleanup(data, OK_EXIT);
 	if (cmd->builtin)
 	{
 		exec_builtins(data, cmd);
-		//ft_close(data);
 		cleanup(data, OK_EXIT);
 	}
 	if (ft_strchr(cmd->argv[0], '/') || (cmd->argv[0][0] == '.'
